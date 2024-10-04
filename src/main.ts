d78ea32baa2ba05b531e6cad854a8c145a02c5ed
Andrew Keats <andrew.keats@everyonetv.co.uk>
2024-10-04 15:27:35 +0100

    wip: actually getting the data to log out

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
+import { CSVIngest } from './ingest';
+
 console.log('Hello World!');
 console.log(process.argv);
+
+const csvIngest = new CSVIngest();
+
+const filePath = process.argv[2];
+const targetFilePath = process.argv[3];
+
+csvIngest.ingest(filePath);
