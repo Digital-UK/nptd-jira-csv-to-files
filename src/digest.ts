84aa831c8cb1bb931c6916c840ba0fc03653ed73
Andrew Keats <andrew.keats@everyonetv.co.uk>
2024-10-04 23:00:52 +0100

    wip: working on transforming the data

--- src/digest.ts
+++ src/digest.ts
@@ -3,10 +3,17 @@ export class DataDigest<T extends Record<any, any>> {
         console.log('DataDigest constructor');
     }
 
-    async digest(data: AsyncGenerator<T>) {
+    async *digest(data: AsyncGenerator<T>) {
         console.log('Digesting data');
         for await (const row of data) {
-            yield record;
+            yield row;
         }
+    }
+}
+
+export async function *digest<T extends Record<any, any>>(data: AsyncGenerator<T>) {
+    console.log('Digesting data');
+    for await (const row of data) {
+        yield row;
     }
 }
