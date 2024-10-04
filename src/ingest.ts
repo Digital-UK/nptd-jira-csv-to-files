421710178aedb8ede15fe7a1ef1322c3154592f8
Andrew Keats <andrew.keats@everyonetv.co.uk>
2024-10-04 13:52:09 +0100

    wip: working on a CSV ingest class

---
+++ src/ingest.ts
@@ -0,0 +1,57 @@
+import type { Parser } from 'csv-parse';
+import { parse } from 'csv-parse';
+
+export class CSVIngest {
+    constructor() {
+        console.log('CSVIngest constructor');
+
+        this.#initParser();
+    }
+
+    async ingest(filePath: string) {
+        console.log('ingest', filePath);
+        // Create read stream
+        // Pipe to parser
+        const readStream = Bun.file(filePath).stream();
+
+        for await (const chunk of readStream) {
+            // Do something with each 'chunk'
+            this.#parser.write(chunk);
+        }
+
+    }
+
+    #initParser() {
+        this.#parser = parse({});
+
+        this.#parser.on('error', function(err){
+            console.error(err.message);
+            process.exit(1);
+        });
+        this.#parser.on('readable', function(this: CSVIngest){
+            let record = this.#parser.read();
+            let processor = this.#dataProcessor
+
+            if (!processor) {
+                console.warn('No data processor set');
+
+                processor = (record) => {
+                    console.log(record);
+                };
+            }
+
+            while (record !== null) {
+                processor(record);
+            }
+        });
+    }
+
+    get #dataProcessor() {
+        return this.#dataProcessorExternal;
+    }
+    
+    #dataProcessorExternal: ((date: any) => void) | undefined;
+    #parser: Parser = parse({});
+    
+
+}
