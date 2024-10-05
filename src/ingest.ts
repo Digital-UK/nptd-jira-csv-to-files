f62a682e3eb560c203de4e8aea7a0bbf0de76314
Andrew Keats <andrew.keats@everyonetv.co.uk>
2024-10-05 01:48:03 +0100

    wip: correcting file path

--- src/ingest.ts
+++ src/ingest.ts
@@ -1,57 +1,11 @@
-import type { Parser } from 'csv-parse';
 import { parse } from 'csv-parse';
-import { async } from './digest';
-
-export class CSVIngest<T extends Record<any, any>> {
-    constructor() {
-        console.log('CSVIngest constructor');
-
-        this.#readablePromiseAndResolvers = Promise.withResolvers();
-        this.#initParser();
-    }
-
-    async *ingest(filePath: string): AsyncGenerator<T> {
-        console.log('Ingesting', filePath);
-        const readStream = Bun.file(filePath).stream();
-        const { promise } = this.#readablePromiseAndResolvers;
-
-        for await (const chunk of readStream) {
-            this.#parser!.write(chunk);
-        }
-
-        await promise;
-
-        this.#parser!.end();
-
-        let record;
-
-        while (record = this.#parser!.read()) {
-            yield record;
-        }
-    }
-
-    #initParser() {
-        this.#parser = parse({
-            columns: true,
-        });
-
-        this.#parser.on('error', (err) => {
-            this.#readablePromiseAndResolvers.reject(err); 
-        });
-        this.#parser.on('readable', () => {
-            this.#readablePromiseAndResolvers.resolve(); 
-        });
-    }
-    
-    #readablePromiseAndResolvers: ReturnType<typeof Promise.withResolvers>;
-    #parser: Parser | undefined;
-}
 
 export async function *ingest<T>(filePath: string): AsyncGenerator<T> {
     const { promise, reject, resolve } = Promise.withResolvers();
     const readStream = Bun.file(filePath).stream();
     const parser = parse({
         columns: true,
+        groupColumnsByName: true
     });
 
     parser.on('error', (err) => {
--- src/transform.ts
+++ src/transform.ts
@@ -3,22 +3,22 @@ export function *transform(data: RowData | unknown): Generator<[filePath: string
     let parentPath = '';
 
     while (true) {
-        const { ['Issue Type']: issueType } = data;
+        const { ['Issue Type']: issueType } = data as RowData;
         const isSubTask = issueType === 'Sub-task';
         
         if (issueType === 'Story') {
-            parentPath = data['Summary'];
+            parentPath = (data as RowData)['Summary'];
         }
 
         if (isSubTask)  {
             const { 
-                ['Labels']: labels,
+                ['Labels']: { [0]: firstLabel },
+                ['Fix Version']: fixVersion,
                 ['File_Name']: fileName, 
                 ['Description']: gherkinDefinition 
             } = data as RowData;
             const issueMarkerIndex = gherkinDefinition.indexOf('@NPTD');
-            data = yield [`${parentPath}/${labels}/${fileName}`, gherkinDefinition.slice(issueMarkerIndex)];
-            //data = yield;
+            data = yield [`${parentPath}/${fixVersion}/${firstLabel}/${fileName}`, gherkinDefinition.slice(issueMarkerIndex)];
         } 
         else {
             data = yield;
